38619e615b1418beafc7a25beaaa4149d3487f5c
MoeinDanA <moein.danesh97@gmail.com>
2019-08-11 18:26:46 +0430

    init files + Server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = 3001;
+
+app.listen(PORT, err => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log(`Running on port ${PORT}`);
+    }
+})
