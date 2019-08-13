5ac4fb1f0e46e4309d8d737072e33223d4c9e5dd
MoeinDanA <moein.danesh97@gmail.com>
2019-08-13 21:26:01 +0430

    bootstrap: static=>package

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,6 @@ const Layout = props => {
                 <meta charSet="utf-8" key='meta_utf8' />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" key="meta_viewport" />
                 <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
-                <link rel="stylesheet" type="text/css" href="/static/vendor/bootstrap/css/bootstrap.min.css"></link>
                 <link rel="stylesheet" type="text/css" href="/static/fonts/font-awesome-4.7.0/css/font-awesome.min.css"></link>
                 <link rel="stylesheet" type="text/css" href="/static/vendor/animate/animate.css"></link>
                 <link rel="stylesheet" type="text/css" href="/static/vendor/css-hamburgers/hamburgers.min.css"></link>
@@ -17,7 +16,6 @@ const Layout = props => {
 
                 <script src="/static/vendor/jquery/jquery-3.2.1.min.js"></script>
 	            <script src="/static/vendor/bootstrap/js/popper.js"></script>
-	            <script src="/static/vendor/bootstrap/js/bootstrap.min.js"></script>
 	            <script src="/static/vendor/select2/select2.min.js"></script>
 	            <script src="/static/vendor/tilt/tilt.jquery.min.js"></script>
                 <script >
